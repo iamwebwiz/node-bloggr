974546bd6852483a9a54defdc287c890cf7ec0a5
iamwebwiz <iamwebwiz@gmail.com>
2019-07-04 19:43:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
