705b5bcccee413c1cb83ba3635820a546b1acc72
iamwebwiz <iamwebwiz@gmail.com>
2019-07-05 09:10:45 +0100

    Add ejs module

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
