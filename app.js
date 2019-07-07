90022cb48864380052cb379d0576493e3ae77782
Ezekiel <iamwebwiz@gmail.com>
2019-07-07 15:21:06 +0100

    Update entry file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const edge = require("express-edge");
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const router = require("./router");
 
 mongoose.connect("mongodb://localhost/bloggie", {
   useNewUrlParser: true
@@ -10,17 +12,15 @@ mongoose.connect("mongodb://localhost/bloggie", {
 
 app.use(edge);
 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("views", `${__dirname}/views`);
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
