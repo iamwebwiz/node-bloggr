373383bc45ff64310b283e241e821d24b439959b
Ezekiel <iamwebwiz@gmail.com>
2019-07-08 06:45:55 +0100

    Add express-fileupload module

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const edge = require("express-edge");
-const app = express();
-const port = process.env.PORT || 3000;
+const fileUpload = require("express-fileupload");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
 const router = require("./router");
 
 mongoose.connect("mongodb://localhost/bloggie", {
@@ -11,6 +12,8 @@ mongoose.connect("mongodb://localhost/bloggie", {
 });
 
 app.use(edge);
+
+app.use(fileUpload());
 
 app.use(bodyParser.json());
 
