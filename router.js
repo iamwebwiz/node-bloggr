83bb612bd04f09aec37fd314610fc6f358154ff9
Ezekiel <iamwebwiz@gmail.com>
2019-07-12 22:34:02 +0100

    Update router.js'

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const PostController = require("./controllers/PostController");
 const FrontendController = require("./controllers/FrontendController");
 
-router.get("/", PostController);
+router.get("/", PostController.index);
 
 router.get("/contact", FrontendController.contact);
 
